e0e372e8b0a7829316bb60fa95cc39e3352b327b
Abetang Joseph <josephabetang@gmail.com>
2019-04-10 00:43:44 +0100

    Write twoNumbersDifference function to pass all previous failing tests

--- src/twoNumbersDiffernce.js
+++ src/twoNumbersDiffernce.js
@@ -1,1 +1,9 @@
-module.exports = function twoNumbersDiffernce(num1, num2) {};
+module.exports = function twoNumbersDiffernce(num1, num2) {
+  if (typeof num1 == "string" || typeof num2 == "string") {
+    return "invalid input, only numbers are allowed";
+  } else if (num1 == null || num2 == null) {
+    return "fill all fields";
+  } else {
+    return num1 - num2;
+  }
+};
