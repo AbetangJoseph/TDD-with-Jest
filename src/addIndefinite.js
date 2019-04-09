3e3d625e0e0c522414c1b9e0dd79dba3cfd45718
Abetang Joseph <josephabetang@gmail.com>
2019-04-09 22:38:28 +0100

    Write addIndefinite function to pass all previous failing tests

--- src/addIndefinite.js
+++ src/addIndefinite.js
@@ -1,1 +1,15 @@
-module.exports = function addIndefinite() {};
+module.exports = function addIndefinite() {
+  let res = 0;
+  if (!arguments.length) {
+    return "input cannot be empty";
+  } else if (arguments.length >= 1) {
+    for (var i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] != "number") {
+        return "invalid, only numbers are allowed";
+      } else {
+        res += arguments[i];
+      }
+    }
+    return res;
+  }
+};
