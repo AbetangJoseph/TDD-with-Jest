83bd1d66316ac7d47178a679bdf07e500429d600
Abetang Joseph <josephabetang@gmail.com>
2019-04-09 09:10:42 +0100

    set up all files structure with no code & test, ready to begin TDD

---
+++ src/allModules.js
@@ -0,0 +1,17 @@
+const addIndifinte = require("./addIndifinte");
+const addTwoNumbers = require("./addTwoNumbers");
+const divideTwoNumbers = require("./divideTwoNumbers");
+const gitHubGetRepo = require("./gitHubGetRepo");
+const multiplyIndifinite = require("./multiplyIndifinite");
+const twoNumbersDiffernce = require("./twoNumbersDiffernce");
+const twoStringsConcat = require("./twoStringsConcat");
+
+module.exports = {
+  addIndifinte,
+  addTwoNumbers,
+  divideTwoNumbers,
+  gitHubGetRepo,
+  multiplyIndifinite,
+  twoNumbersDiffernce,
+  twoStringsConcat
+};
---
+++ test/tests.unit.test.js
@@ -0,0 +1,9 @@
+const {
+  addIndifinte,
+  addTwoNumbers,
+  divideTwoNumbers,
+  gitHubGetRepo,
+  multiplyIndifinite,
+  twoNumbersDiffernce,
+  twoStringsConcat
+} = require("../src/allModules");
