ef4a95c8e425f64c1a0515458613b52c8c3b8264
Abetang Joseph <josephabetang@gmail.com>
2019-04-10 01:02:03 +0100

    Write multiplyIndifinite function to pass all previous failing tests

--- src/multiplyIndifinite.js
+++ src/multiplyIndifinite.js
@@ -1,1 +1,15 @@
-module.exports = function multiplyIndifinite() {};
+module.exports = function multiplyIndifinite() {
+  let res = 1;
+  if (!arguments.length) {
+    return "fill all fields";
+  } else if (arguments.length >= 1) {
+    for (var i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] != "number") {
+        return "invalid, only numbers are allowed";
+      } else {
+        res *= arguments[i];
+      }
+    }
+    return res;
+  }
+};
